1f2a92f18b4c62ae583cb45e3e971c512f4758f5
justice1k <justiceakpalu11@gmail.com>
2025-02-22 00:41:37 +0000

    countries 2.18

--- part2/countries/src/Info.jsx
+++ part2/countries/src/Info.jsx
@@ -21,12 +21,27 @@ const Info = ({names}) => {
         )
     }else if(names.length === 1 && details){
         
-        return <>{details.flag}</>
+        return (
+            <div>
+                <h1>{details.name.common}</h1>
+                <p>Capital {details.capital}</p>
+                <p>Area {details.area}</p>
+                <h2>Languages</h2>
+                {Object.entries(details.languages).map(([code,name]) => <li key={code}>{name}</li>)}
+                <p>{details.flag}</p>
+            </div>
+        )
+    }else if(names.length === 0){
+        return(
+            <div>
+                Country not found...
+            </div>
+        )
     }
 
     return(
         <div>
-            {names.map((item) => <p>{item}</p>)}
+            {names.map((item,index) => <ul key={index}>{item}</ul>)}
         </div>
     )
     
